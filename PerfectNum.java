b44f4290b4311c7b19947eb29fa8306aa93003b3
Vanshika Thakre <vanshikathakre4@gmail.com>
2026-08-26 19:40:37 +0530

    day 6

---
+++ PerfectNum.java
@@ -0,0 +1,16 @@
+public class PerfectNum{
+    public static void main(String[] args) {
+        int n = 9 ;
+        int sum = 0 ;
+
+        for(int i = 1 ; i<=n/2 ; i++){
+             if(n%i == 0){
+                sum = sum +i;
+             }
+        }
+        if(sum == n ){
+            System.out.println("Perfect Number");
+        }else System.out.println("Not perfect number");
+
+    }
+}
---
+++ Prime1toN.java
@@ -0,0 +1,11 @@
+public class Prime1toN {
+    public static void main(String[] args) {
+        int n = 5;
+        String[] ans = new String[n];
+        for(int i = 0 ; i<=n ;i++){
+            if(i % 2 == 0){
+                
+            }
+        }
+    }
+}
